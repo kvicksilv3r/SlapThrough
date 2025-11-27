5ac02d9775920fbb9583968a5e6afa721e22cc45
Dogge <douglas.ekehammar@gmail.com>
2025-11-27 17:32:26 +0100

    Movement is vector and directional boost

--- SlapThrough/Assets/Player.cs
+++ SlapThrough/Assets/Player.cs
@@ -3,110 +3,116 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    public CharacterController characterController;
+	public CharacterController characterController;
 
-    public float jumpPower = 10f;
-    public float gravity = 5f;
+	public float jumpPower = 10f;
+	public float gravity = 5f;
 
-    private float horizontalVelocity;
-    private float verticalVelocity;
+	private Vector3 horizontalVelocity;
+	private Vector3 verticalVelocity;
 
-    private float joystick;
+	private float joystick;
 
-    public float acceleration;
-    public float deceleration;
-    public float maxSpeed;
+	public float acceleration;
+	public float deceleration;
+	public float maxSpeed;
 
-    public float horizontalControl;
+	public float horizontalControl;
 
-    public float groundControl = 1;
-    public float airControl = 0.5f;
+	public float groundControl = 1;
+	public float airControl = 0.25f;
 
-    private bool justJumped = false;
+	public float directionChangeBoost = 2;
 
-    public float deadzone = 0.05f;
+	private bool justJumped = false;
 
-    public float direction;
+	public float deadzone = 0.05f;
 
-    void Start()
-    {
+	public float direction;
 
-    }
+	void Start()
+	{
 
-    // Update is called once per frame
-    void Update()
-    {
-        HandleVertical();
-        HandleHorizontal();
+	}
 
-        Vector3 totalVelocity = new Vector3(horizontalVelocity, verticalVelocity);
-        characterController.Move(totalVelocity * Time.deltaTime);
+	// Update is called once per frame
+	void Update()
+	{
+		HandleVertical();
+		HandleHorizontal();
 
-        if (justJumped)
-        {
-            justJumped = false;
-        }
-    }
+		Vector3 totalVelocity = horizontalVelocity + verticalVelocity;
+		characterController.Move(totalVelocity * Time.deltaTime);
 
-    private void HandleVertical()
-    {
-        if (characterController.isGrounded)
-        {
-            if (!justJumped)
-            {
-                verticalVelocity = -1;
-            }
-        }
+		if (justJumped)
+		{
+			justJumped = false;
+		}
+	}
 
-        else
-        {
-            verticalVelocity += -1 * gravity * Time.deltaTime;
-        }
-    }
+	private void HandleVertical()
+	{
+		if (characterController.isGrounded)
+		{
+			if (!justJumped)
+			{
+				verticalVelocity = Vector3.down;
+			}
+		}
 
-    private void HandleHorizontal()
-    {
-        if (characterController.isGrounded)
-        {
-            horizontalControl = groundControl;
-        }
-        else
-        {
-            horizontalControl = airControl;
-        }
+		else
+		{
+			verticalVelocity += Vector3.down * gravity * Time.deltaTime;
+		}
+	}
 
-        if (Mathf.Abs(joystick) > deadzone)
-        {
-            if (joystick > 0) { direction = 1; }
-            if (joystick < 0) { direction = -1; }
+	private void HandleHorizontal()
+	{
+		if (characterController.isGrounded)
+		{
+			horizontalControl = groundControl;
+		}
+		else
+		{
+			horizontalControl = airControl;
+		}
 
-            Vector3 test = Vector3.MoveTowards(new Vector3(horizontalVelocity, 0), Vector3.right * maxSpeed * direction, acceleration * horizontalControl * Time.deltaTime);
-            horizontalVelocity = test.x;
-        }
+		var directionDifference = Vector3.Dot(horizontalVelocity.normalized, (Vector3.right * direction).normalized);
 
-        else
-        {
-            //decelerate
-            Vector3 test = Vector3.MoveTowards(new Vector3(horizontalVelocity, 0), Vector3.zero, deceleration * horizontalControl * Time.deltaTime);
-            horizontalVelocity = test.x;
-        }
-    }
+		var directionalBoost = directionDifference < 0 ? directionChangeBoost : 1;
 
-    public void OnJump()
-    {
-        if (!characterController.isGrounded)
-        {
-            print("NotGrounded");
-            return;
-        }
 
-        print("Yump");
-        verticalVelocity = jumpPower;
-        justJumped = true;
-    }
 
-    public void OnMove(InputValue input)
-    {
-        joystick = input.Get<Vector2>().x;
-    }
+		if (Mathf.Abs(joystick) > deadzone)
+		{
+			if (joystick > 0) { direction = 1; }
+			if (joystick < 0) { direction = -1; }
+
+			horizontalVelocity = Vector3.MoveTowards(horizontalVelocity, Vector3.right * maxSpeed * direction, acceleration * directionalBoost * horizontalControl * Time.deltaTime);
+		}
+
+		else
+		{
+			//decelerate
+			horizontalVelocity = Vector3.MoveTowards(horizontalVelocity, Vector3.zero, deceleration * horizontalControl * Time.deltaTime);
+		}
+	}
+
+	public void OnJump()
+	{
+		if (!characterController.isGrounded)
+		{
+			print("NotGrounded");
+			return;
+		}
+
+		print("Yump");
+		verticalVelocity = Vector3.up * jumpPower;
+		justJumped = true;
+	}
+
+	public void OnMove(InputValue input)
+	{
+		joystick = input.Get<Vector2>().x;
+	}
 }
